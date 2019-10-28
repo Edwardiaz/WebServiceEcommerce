11685d24ae23b67316f9bc1a28e7b6e30cd3d5bd
Rogelio <Roger.Alonso7412@gmail.com>
2019-10-28 16:49:22 -0600

    Cross Origin Annotation

--- ecommerce/src/main/java/com/ecommerce/controller/RoleController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/RoleController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import com.ecommerce.service.IOptionssService;
 import com.ecommerce.service.IRoleOptionsService;
 import com.ecommerce.service.IRoleService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api")
 public class RoleController {
