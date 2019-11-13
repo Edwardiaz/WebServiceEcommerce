ddf59e90d110e05f363bbeea728f53f827b88635
edwardiaz <jorgediaz9515@gmail.com>
2019-11-13 16:54:39 -0600

    Revert "sfds"
    This reverts commit bfb765543eb95c4e2e5c7ef5f26861c730890958.

---
+++ ecommerce/src/main/java/com/ecommerce/entity/ProductsCategory.java
@@ -0,0 +1,169 @@
+//package com.ecommerce.entity;
+//
+//import java.io.Serializable;
+//
+//import javax.persistence.Basic;
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "productsCategory")
+//public class ProductsCategory implements Serializable{
+//
+//	private static final long serialVersionUID = 1L;
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Basic(optional = false)
+//	@Column(name = "idProductCategory")
+//	private Long idProductsCategory;
+//	@Column(name = "idProducts")
+//	private Long idPro;
+//	@Column(name = "idCategory")
+//	private Long idCat;
+//	
+//	
+//	@JoinColumn(name = "idCategory", insertable = false, updatable = false)
+//	@ManyToOne
+//	private Category category;
+//
+//	@JoinColumn(name = "idProducts", insertable = false, updatable = false)
+//	@ManyToOne
+//	private Products products;
+//	
+//	public ProductsCategory() {
+//		
+//	}
+//	
+//	public ProductsCategory(Long idProductsCategory, Long idPro, Long idCat) {
+//		super();
+//		this.idProductsCategory = idProductsCategory;
+//		this.idPro = idPro;
+//		this.idCat = idCat;
+//	}
+//
+//	public Long getIdProductsCategory() {
+//		return idProductsCategory;
+//	}
+//
+//	public void setIdProductsCategory(Long idProductsCategory) {
+//		this.idProductsCategory = idProductsCategory;
+//	}
+//
+//	public Long getIdPro() {
+//		return idPro;
+//	}
+//
+//	public void setIdPro(Long idPro) {
+//		this.idPro = idPro;
+//	}
+//
+//	public Long getIdCat() {
+//		return idCat;
+//	}
+//
+//	public void setIdCat(Long idCat) {
+//		this.idCat = idCat;
+//	}
+//
+//	public Category getCategory() {
+//		return category;
+//	}
+//
+//	public void setCategory(Category category) {
+//		this.category = category;
+//	}
+//
+////	public Products getProducts() {
+////		return products;
+////	}
+////
+////	public void setProducts(Products products) {
+////		this.products = products;
+////	}
+//
+////	public ProductsCategory(Long idProductsCategory) {
+////		this.idProductsCategory = idProductsCategory;
+////	}
+//
+//	
+//	
+//}
+
+
+package com.ecommerce.entity;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import com.fasterxml.jackson.annotation.JsonInclude;
+@Entity
+@Table(name = "productsCategory")
+public class ProductsCategory implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "idProductCategory")
+	private Long idProductsCategory;
+	@Column(name = "idProducts")
+	private Long idPro;
+	@Column(name = "idCategory")
+	private Long idCat;
+	@JsonInclude
+	@Transient
+	private String name;
+	@JoinColumn(name = "idCategory", insertable = false, updatable = false)
+	@ManyToOne
+	private Category category;
+	@JoinColumn(name = "idProducts"/*, insertable = false, updatable = false*/)
+	@ManyToOne
+	private Products products;
+	
+	public ProductsCategory() {
+	}
+	public ProductsCategory(Long idProductsCategory, Long idPro, Long idCat) {
+		super();
+		this.idProductsCategory = idProductsCategory;
+		this.idPro = idPro;
+		this.idCat = idCat;
+	}
+	public Long getIdProductsCategory() {
+		return idProductsCategory;
+	}
+	public void setIdProductsCategory(Long idProductsCategory) {
+		this.idProductsCategory = idProductsCategory;
+	}
+	public Long getIdProducts() {
+		return idPro;
+	}
+	public void setIdProducts(Long idProducts) {
+		this.idPro = idProducts;
+	}
+	public Long getIdCategory() {
+		return idCat;
+	}
+	public void setIdCategory(Long idCategory) {
+		this.idCat = idCategory;
+	}
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+}
